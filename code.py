6705270d465ebcddadd68c77e88895fd9a411d13
lex-koelewijn <lex.koelewijn@gmail.com>
2019-12-22 19:38:00 +0100

    Added a simpler implementation using one hot vectors with much better performance

--- code.py
+++ code.py
@@ -50,8 +50,8 @@ print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 # -
-# ## One Hot Vectors
-# Train a linear regression again, but this time use One Hot encodings. 
+# ## One Hot Vector With Reshaping Of Data
+# Train a linear regression again, but this time use One Hot encodings. In this implementation data is restructured but this seems to harm performance as you cannot iteratively train the linear model. 
 
 # +
 from sklearn.preprocessing import LabelEncoder
@@ -74,6 +74,7 @@ regressor = LinearRegression()
 for i in range(0,X_train.shape[0]):
     regressor.fit(X_train[i,:,:], y_train[i])
 
+#TODO: I am unsure you can train a model iterativly as I'm doing now.     
 #Show the predicted coefficients
 coeff_df = pd.DataFrame(regressor.coef_, columns=['Coefficient'])
 # #Coeffecients currently represent which of the past 20 notes in the window is most influential for the prediction.
@@ -97,6 +98,45 @@ def one_hot_to_original_data(y_onehot,y):
         y_orig[i] = np.unique(y)[np.argmax(y_onehot[i,:])]
     return y_orig
 
+
+# +
+#Convert back to integer notes by taking the note with the maximum proability. 
+y_test_orig = one_hot_to_original_data(y_test, y)
+y_pred_orig = one_hot_to_original_data(y_pred, y)
+
+# Show The difference between actual values and predicted values
+df = pd.DataFrame({'Actual': y_test_orig, 'Predicted': y_pred_orig})
+print(df)
+
+from sklearn import metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test_orig, y_pred_orig))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test_orig, y_pred_orig))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test_orig, y_pred_orig)))
+# -
+# # One Hot Vector Without Reshaping Data
+#
+# In this version of the one hot vector incoding the data is not being reshaped, so we use the original concatenated input vector for training. This model has similar/slightly better performance than the simple linear regression using the notes as data.
+
+# +
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+#Create One hot encodings of X and y data. 
+onehot_encoder = OneHotEncoder(sparse=False)
+y_onehot = onehot_encoder.fit_transform(y.reshape(len(y),1))
+X_onehot = onehot_encoder.fit_transform(X)
+
+
+
+# +
+X_train, X_test, y_train, y_test = train_test_split(X_onehot, y_onehot, test_size=0.2, random_state=0)
+
+#Train a linear model by going through each window of training data seperately. 
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#Make predictions for test data 
+y_pred = regressor.predict(X_test)
 
 # +
 #Convert back to integer notes by taking the note with the maximum proability. 
